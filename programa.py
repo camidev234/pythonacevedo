3582384bdb0f7ac720b29397ed6b90c037968866
camilo <camilo222andresace@gmail.com>
2023-04-24 16:57:02 -0500

    version 7

--- programa.py
+++ programa.py
@@ -1,3 +1,12 @@
-numeros = [int(input("Escriba un numero: ")), int(input("Escriba otro numero:")), int(input("Escriba otro numero:"))]
+numero1 = int(input("Escriba el numero 1: "))
+numero2 = int(input("Escriba el numero 2: "))
+numero3 = int(input("Escriba el numero 3: "))
+numeromax =0
 
-print(max(numeros))
+if numero1 > numero2:
+    if numero1 > numero3:
+        print(f"El numero mayor es {numero1}")
+    else:
+        print(f"El numero mayor es {numero3}")
+else:
+    print(f"El numero mayor {numero2}")
