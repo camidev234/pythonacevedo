06eda21e4bcbbabac0afa8bcb01a4b0c725835f8
camilo <camilo222andresace@gmail.com>
2023-04-26 16:13:07 -0500

    Version actualizada

--- ejerciciosEnClase/factorial.py
+++ ejerciciosEnClase/factorial.py
@@ -2,5 +2,8 @@ numUno = int(input("Escriba el numero uno: "))
 numDos = int(input("Escriba el numero dos: "))
 
 while numUno % numDos == 1 or numDos % numUno == 1:
+    print(f"{numUno} y {numDos} no son factor")
     numUno = int(input("Escriba el numero uno: "))
     numDos = int(input("Escriba el numero dos: "))
+
+print(f"{numUno} y {numDos} si son factor")
