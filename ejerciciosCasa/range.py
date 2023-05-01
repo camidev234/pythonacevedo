446ec870fb5bb0380213f2496db9541630e8145d
Camilo Acevedo <camilo222andresace@gmail.com>
2023-04-30 20:38:22 -0500

    Ejercicio con funcion random

---
+++ ejerciciosCasa/range.py
@@ -0,0 +1,20 @@
+import random
+
+aleatorio = random.randrange(1, 50, 1)
+print(
+    "------------------"
+    "Adivine el numero"
+    "------------------"
+)
+numero = int(input("Escriba el numero: "))
+
+print(aleatorio)
+
+while numero!=aleatorio:
+    numero = int(input("Escriba el numero: "))
+    if numero < aleatorio:
+        print("El numero que usted introdujo es menor al numero aleatorio")
+    elif numero > aleatorio:
+        print("El numero aleatorio es menor")
+    else:
+        print("¡El numero es correcto!")
