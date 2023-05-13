e9a012bcf34c3b0a40b21bd8c678475b4c17ae31
Camilo Acevedo <camilo222andresace@gmail.com>
2023-05-13 18:30:55 -0500

    ejercicio 11 con funciones añadido

---
+++ miscelanea2Funciones/funciones7.py
@@ -0,0 +1,22 @@
+import random
+def llenar(tamaño):
+    lista=[]
+    lista=[random.randrange(2, 15) for i in range(tamaño)]
+    return lista
+tam= random.randint(1, 20)
+listado= llenar(tam)
+print(listado)
+
+def hallarFactoriales(lista):
+    operacion=0
+    for j in lista:
+        numero=j
+        operacion=0
+        for k in range(j-1, 1, -1):
+            operacion=j*k
+            j=operacion
+        j=numero
+        print(f"El factorial de {j} es {operacion}")
+        
+            
+hallarFactoriales(listado)
