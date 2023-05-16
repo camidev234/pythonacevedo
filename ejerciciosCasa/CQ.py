5776f5023d037ed03a0920f4eac8be83163894a8
Camilo Acevedo <camilo222andresace@gmail.com>
2023-05-15 21:35:59 -0500

    CQ empezado

--- ejerciciosCasa/CQ.py
+++ ejerciciosCasa/CQ.py
@@ -8,7 +8,7 @@ def llenarLista(tamaño):
 listado = llenarLista(lon)
 print(listado)
 
-listado = [1,2,3,4,5,6,7,7,8,8]
+listado = [1,2,3,4,5,6,7,7,8,8,10,11]
 
 
 def ordenAsc(lista):
@@ -23,28 +23,27 @@ def ordenAsc(lista):
 print(ordenAsc(listado))
 
 def Cuartil(lista):
-    i=1
     listaCuartil=[]
     cuart2=0
     cuart=0
     posicion=0
     op=0
+    op2=0
     n=len(lista)
     for m in range(1,4):
-        posicion=cuart2
-        cuart= (i * (n+1))/4
+        posicion=op2
+        cuart= (m * (n+1))/4
+        print(cuart)
         cuart2=int(cuart)
-        op= (cuart2 + (cuart2-1))/2
-        print(cuart, cuart2, op)
-        
+        o=cuart2+1
+        op=(cuart+o)/2
+        op2=int(op)
+        print(op2)
         if m == 1:
-            listaCuartil=lista[ :cuart2]
-            print(f"Cuartil {i} {listaCuartil}")
+            listaCuartil=lista[ :op2]
+            print(f"Cuartil {m} {listaCuartil}")
         else:
-            listaCuartil=lista[posicion:cuart2]
-            print(f"Cuartil {i} {listaCuartil}")
-    
-            
-        i+=1
+            listaCuartil=lista[posicion:op2]
+            print(f"Cuartil {m} {listaCuartil}")
 
 Cuartil(listado)
