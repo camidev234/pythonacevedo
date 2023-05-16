1f789eabd42cbebb013960fa7d0c7602d6f37561
Camilo Acevedo <camilo222andresace@gmail.com>
2023-05-15 20:59:40 -0500

    adelanto CQ

--- ejerciciosCasa/CQ.py
+++ ejerciciosCasa/CQ.py
@@ -8,7 +8,7 @@ def llenarLista(tamaño):
 listado = llenarLista(lon)
 print(listado)
 
-
+listado = [1,2,3,4,5,6,7,7,8,8]
 
 
 def ordenAsc(lista):
@@ -22,45 +22,28 @@ def ordenAsc(lista):
 
 print(ordenAsc(listado))
 
-
-def mediana(lista):
-    listamed1=[]
-    listamed2=[]
-    if len(lista) %2!=0:
-        pos = (len(lista)+1)//2
-        listamed1=lista[ :pos]
-        listamed2=lista[pos: ]
-        print(listamed1)
-        print(listamed2)
-        return f"La mediana de la lista es: {lista[pos-1]}"
-    else:
-        pos = (len(lista) - 1) // 2
-        pos1 = (lista [pos])
-        pos2 = (lista[pos + 1])
-        pos = pos1 + pos2
-        pos = pos / 2
-        return pos
+def Cuartil(lista):
+    i=1
+    listaCuartil=[]
+    cuart2=0
+    cuart=0
+    posicion=0
+    op=0
+    n=len(lista)
+    for m in range(1,4):
+        posicion=cuart2
+        cuart= (i * (n+1))/4
+        cuart2=int(cuart)
+        print(cuart, cuart2)
+        if n % 2 !=0:
+            if m == 1:
+                listaCuartil=lista[ :cuart2]
+                print(f"Cuartil {i} {listaCuartil}")
+            else:
+                listaCuartil=lista[posicion:cuart2]
+                print(f"Cuartil {i} {listaCuartil}")
     
-media = mediana(listado)
-print(f"La mediana de la lista es: {media}")
+            
+        i+=1
 
-
-
-# def Cuartil(lista):
-#     i=1
-#     listaCuartil=[]
-#     cuart2=0
-#     cuart=0
-#     posicion=0
-#     op=0
-#     n=len(lista)
-#     for m in range(1,4):
-#         posicion=cuart2
-#         cuart= (i * (n+1))/4
-#         cuart2=int(cuart)
-#         print(cuart, cuart2)
-        
-              
-#         i+=1
-
-# Cuartil(listado)
+Cuartil(listado)
