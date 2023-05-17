90e27876fb6101897ca13221a5e381d900e8e74d
camilo <camilo222andresace@gmail.com>
2023-05-17 17:08:04 -0500

    tuplas y diccionarios

--- ejerciciosCasa/CQ.py
+++ ejerciciosCasa/CQ.py
@@ -1,5 +1,5 @@
 import random
-lon = random.randrange(200, 2500)
+lon = random.randrange(50,99)                            
 def llenarLista(tamaño):
     lista=[]
     lista=[random.randint(100, 500) for i in range(tamaño)]
@@ -8,7 +8,7 @@ def llenarLista(tamaño):
 listado = llenarLista(lon)
 print(listado)
 
-# listado=[1,2,3,4,5,6,7,8,9,10,11]
+# listado=[1,2,3,4,5,6,7,8,9,10,11,12,13]
 
 def ordenAsc(lista):
     for j in range(len(lista)):
@@ -20,53 +20,92 @@ def ordenAsc(lista):
     return f"la lista ordenada en orden ascendente: {lista}"
 
 print(ordenAsc(listado))
-
-def cuartil(lista):
+def cuartil(lista, lista1, lista2,lista3,lista4):
     formula=0
     n = len(lista)
     listaCuartil=[]
-    conv=0
-    # posicionInicial=0
+    redondeo=0
     for k in range(1, 4):
         if len(lista) % 2!=0 or len(lista) % 2 == 0:
-            # posicionInicial=conv
+            pos2=redondeo
             formula=k*(n+1) / 4
-            conv=round(formula)
-            pos=lista[conv-1]
-            print(f"Q{k} = {formula} valor en lista {pos}")
-            listaCuartil.append(formula)
-            print(listaCuartil)
-            # if k == 1:
-            #     listaCuartil=lista[ :conv-1]
-            #     listaCuartil.append(formula)
-            #     print(f"Cuartil {k} {listaCuartil}")
-            # elif k == 2:
-            #     listaCuartil = lista[posicionInicial-1:conv]
-            #     print(f"Cuartil {k} {listaCuartil}")
-            # else:
-            #     listaCuartil = lista[posicionInicial:conv]
-            #     print(f"Cuartil {k} {listaCuartil}")
+            redondeo= round(formula)
+            r2=redondeo+1
+            promedio= (redondeo+r2)/2 
+            posicion=lista[redondeo-1]           
+            if k == 1:
+                listaCuartil=lista[ :redondeo]
+                print(f"cuartil {k} {listaCuartil}")
+                lista1= listaCuartil
+            elif k == 2:
+                listaCuartil=lista[pos2:redondeo]
+                lista2=listaCuartil
+                print(f"cuartil {k} {listaCuartil}")
+            elif k == 3:
+                listaCuartil=lista[pos2:redondeo]
+                lista3=listaCuartil
+                print(f"cuartil {k} {listaCuartil}")
+                lista4=lista[redondeo: ]
             
-    return "Fin de procedimiento de cuartiles"
+            print(f"Q{k} = {formula} valor: {posicion}")
             
-print(cuartil(listado))
+    return lista1,lista2,lista3,lista4
+cuartil1=[]
+cuartil2=[]
+cuartil3=[]
+cuartil4=[]            
+c1,c2,c3,c4= cuartil(listado,cuartil1,cuartil2,cuartil3,cuartil4)
+
+print(f"Ultima {c4}")
+
+def promCuartil(lista):
+    sum=0
+    long = len(lista)
+    for h in lista:
+        sum+=h
+    promedio= sum/long
+    return promedio
+
+print(f"El promedio de la lista 1 es:", promCuartil(c1))
+print(f"El promedio de la lista 2 es:", promCuartil(c2))
+print(f"El promedio de la lista 3 es:", promCuartil(c3))
+print(f"El promedio de la lista 3 es:", promCuartil(c4))
+
+
 
 def quintil(lista):
     formula=0
     n = len(lista)
     listaCuartil=[]
-    conv=0
-    # posicionInicial=0
     for k in range(1, 5):
         if len(lista) % 2!=0 or len(lista) % 2 == 0:
-            # posicionInicial=conv
             formula=(k*(n+1)) / 5
-            conv=round(formula)
-            pos=lista[conv-1]
-            print(f"k{k} = {formula} valor en lista {pos}")
+            redondeo= round(formula)
+            r2=redondeo+1
+            promedio= (redondeo+r2)/2
+            print(f"Promedio {promedio}") 
+            posicion=lista[redondeo-1]  
+            print(f"k{k} = {formula} valor {posicion}")
             listaCuartil.append(formula)
             print(listaCuartil)
 
     return "Fin de procedimiento de quintiles"
 print(quintil(listado))
+
+
+# def buscarCuartil(cuartil, lista):
+#     formula=0
+#     n = len(lista)
+#     if cuartil > 3:
+#         print(f"El cuartil {cuartil} no existe")
+#     else:
+#         formula = cuartil*(n+1)/4
+#         redondeo= round(formula)
+#         r2=redondeo+1
+#         promedio= (redondeo+r2)/2 
+#         posicion=lista[redondeo-1]   
+
+#     return f"El cuartil {cuartil} es {formula} valor: {posicion}"
+
+# print(buscarCuartil(int(input("Escriba el cuartil que desea buscar: ")), listado))
         
