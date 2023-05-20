a2f9e4568a693561f7716898c131eb37ded7c589
Camilo Acevedo <camilo222andresace@gmail.com>
2023-05-19 20:44:01 -0500

    diccionarios (19/05/2023)

--- ejerciciosCasa/diccionarios3.py
+++ ejerciciosCasa/diccionarios3.py
@@ -29,13 +29,42 @@ def enes(reps, clave, valor, diccionario):
 print(enes(int(input("Escriba cuantas veces quiere llenar el diccionario: ")), input("Escriba la clave: "), input("Escriba el valor: "), inglesEspañol))
 
 def sepDic(diccionario):
-    # t= diccionario.items()
-    # return t
+    #FORMA 1
+    """print()
     print(f"Tuplas del diccionario {diccionario}")
+    t= diccionario.items()
+    return t"""
+    #FORMA 2
+    """print(f"Tuplas del diccionario {diccionario}")
     for k in diccionario.items():
         t = k
         print(t)
     print()
+    return True"""
+    #FORMA 3
+    valores=[]
+    claves=[]
+    print("---------------------------------------------------------------")
+    for h in diccionario:
+        claves.append(h)
+        c = diccionario[h]
+        valores.append(c)
+    tc=()
+    for h in claves:
+        if h in diccionario:
+            tc+=(h,)
+    print(f"Tupla de claves {tc}")
+    tv=()
+    for j in valores:
+        tv+=(j,)
+    print(f"Tupla de valores {tv}")
+    
     return True
+        
+                   
+    
 sepDic(españolIngles)
 sepDic(inglesEspañol)
+
+
+
