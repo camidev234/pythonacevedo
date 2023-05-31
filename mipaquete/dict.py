056fecb2b64a193c931da0609ff86f4a104035bb
camilo <camilo222andresace@gmail.com>
2023-05-31 17:13:34 -0500

    paquete de listas y diccionarios

---
+++ mipaquete/dict.py
@@ -0,0 +1,38 @@
+#llenar diccionario
+dict1={}
+
+def completeDict(diccionario):
+    reps = int(input("Escriba cuantas veces quiere llenar el diccionario: "))
+    for h in range(1, reps+1):
+        clave = input("Escriba la clave: ")
+        valor= input("Escriba un valor: ")
+        diccionario.update({clave:valor})
+
+    return diccionario
+
+
+# separar diccionario en dos tuplas una de claves y otra de valores
+
+
+def sepDic(diccionario):
+    valores=[]
+    claves=[]
+    print("---------------------------------------------------------------")
+    for h in diccionario:
+        claves.append(h)
+        c = diccionario[h]
+        valores.append(c)
+    tc=()
+    for h in claves:
+        if h in diccionario:
+            tc+=(h,)
+    tv=()
+    for j in valores:
+        tv+=(j,)
+    
+    return tc, tv
+
+
+
+
+
---
+++ prueba.py
@@ -0,0 +1,9 @@
+import mipaquete.listas as ml
+
+lista1 = []
+
+lista1 = ml.completeListRandom(17, 10, 50)
+print(lista1)
+ml.hallarFactoriales(lista1)
+print(ml.buscaImpares(lista1))
+print(ml.primos(lista1))
