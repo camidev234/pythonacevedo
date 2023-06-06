a755e4348048ebfe462dedcd0040d11b9666aa9b
Camilo Acevedo <camilo222andresace@gmail.com>
2023-06-06 09:10:18 -0500

    ejercicio Salarios

--- ejerciciosEnClase/ejercicio6junioSlarios.py
+++ ejerciciosEnClase/ejercicio6junioSlarios.py
@@ -1,9 +1,13 @@
 class Empleado:
+    contador = 0
+    sumaSalarios = 0
     def __init__(self, nombre, salario, cargo):
-          self.__nombre = nombre
-          self.__salario = salario
-          self.__cargo = cargo
-
+        self.__nombre = nombre
+        self.__salario = salario
+        self.__cargo = cargo
+        Empleado.contador +=1
+        Empleado.sumaSalarios += self.__salario
+    
     def getName(self):
         return self.__nombre
     
@@ -33,12 +37,40 @@ class Empleado:
         if self.__salario > 1160000:
             ipc = self.__salario * 13.12 / 100
             incremento = self.__salario + ipc
+            self.__salario = incremento
+            return ipc
         else:
             ipc = self.__salario * 16.12 / 100
             incremento = self.__salario + ipc
+            self.__salario = incremento
+            return ipc
+            
 
-        return incremento
+    
+    def getSuma(self):
+        return Empleado.sumaSalarios
+    
+    def promedioSalarios(self):
+        promSalarios = Empleado.sumaSalarios / Empleado.contador
+        return promSalarios
+    
+    def horasExtras(self, horas):
+        if horas > 40:
+            return f"No se admiten mas de 2 horas diarias"
+        else:
+            suma = 4833 * horas
+            self.__salario = self.__salario + suma
+            return suma
 
 
-empleado = Empleado("Juan", 1200000,"ñero")
-print(empleado.incrementoSalario())
+# uno = Empleado("Juan", 1162000, "dd")
+# uno.incrementoSalario()
+# uno.horasExtras(9)
+# print(uno.getSalary())
+# dos = Empleado("Juan", 1160000, "dd")
+# print(dos.horasExtras(9))
+# print(dos.incrementoSalario())
+# print(dos.getSalary())
+# print(dos.getSuma())
+# print(uno.promedioSalarios())
+
