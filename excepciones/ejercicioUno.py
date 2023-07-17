c740e04776bfde1407e54713abf18a9c0844d6b6
juand <juandiegoimbachihoyos@gmail.com>
2023-07-17 16:37:32 -0500

    funcion cuadratica con excepciones

---
+++ excepciones/ejercicioUno.py
@@ -0,0 +1,35 @@
+
+#Funcion cuadratica con try except
+import math
+try:
+    print("""
+    1. Suma
+    2-. Restemelo
+    """)
+
+    op = float(input("Ingrese la operacion que desea realizar: "))
+    a = float(input("Digite el numero a: "))
+    b = float(input("Digite el numero b: "))
+    c = float(input("Digite el numero c: "))
+
+    operacionUno  = b ** 2 - (4*a*c)
+    if operacionUno < 0.0:
+        raise Exception("Indeterminacion")
+    rc = math.sqrt(operacionUno) 
+    match op:
+        case 1:
+            opDos = -b + rc
+            opTres = opDos / (2*a)
+            print(f"El resultado de la funcion cuadratica: {opTres}")
+        case 2:
+            opDos = -b - rc
+            opTres = opDos / (2*a)
+            print(f"El resultado de la funcion cuadratica: {opTres}")
+except ValueError:
+    print("Solo numeros")
+except Exception as e:
+    print(f"Error: {e}")
+except:
+    print("Hubo un error durante la ejecucion.")
+
+
