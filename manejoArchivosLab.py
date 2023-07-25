c70952be97f2e734ea5264990989b8f59f0c10be
Camilo Acevedo <camilo222andresace@gmail.com>
2023-07-24 22:47:22 -0500

    lab 4.3.1.15 comentado v1

---
+++ manejoArchivosLab.py
@@ -0,0 +1,52 @@
+from os import strerror
+
+contadorLetras = 0
+contadorNumeros = 0
+contadorCs = 0
+
+# Inicializa 26 contadores para cada letra latina.
+# Nota: hemos usado una comprensión para esto.
+"""
+la comprension hace uso de un diccionario, usando la funcion chr recibe cada letra del abecedario las cuales
+las cuales mediante un for van en un rango de la a la z ordenadamente, es decir, la variable iteradora ch es cada letra del
+alfabeto y se inicializa con un valor de 0 por defecto
+
+Hay que tener en cuenta que la variable ch tomara un entero, el cual sera el valor unicode para cada letra
+
+"""
+counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
+# se ingresa mediante teclado el nombre del archivo
+file_name = input("Ingresa el nombre del archivo a analizar: ")
+# se inicializa la estructura try-except
+try:
+    # se realiza la apertura del archivo en modo de lectura
+    file = open(file_name, "rt")
+    # se hace un bucle for que recorrera el archivo (variable file) linea por linea
+    for line in file:
+        # se anida un for que para cada linea (variable line) recorrera los caracteres
+        for char in line:
+            # Si el caracter es una letra:
+            if char.isalpha():
+                """
+                Esa letra se convierte a minuscula y en el diccionaro counters para la clave 
+                cuyo valor sea igual al caracter que es alfabetico (char), para esa letra se aumenta su
+                contador en uno, es decir, su aparicion en el archivo incrementara en uno.
+                
+                """
+                counters[char.lower()] += 1
+                # se cuentan las letras en general
+                contadorLetras +=1
+    # se cierra el archivo
+    file.close()
+    # Ahora se recorre el diccionario 
+    for char in counters.keys():
+        # se asigna a la variable c el valor de cada clave, es decir el valor que tiene char (variable iteradora) 
+        c = counters[char]
+        # if esa letra tiene un VALOR mayor a 0 osea aparecio al menos una vez:
+        if c > 0: 
+            # se imprime la clave asociando su valor osea la cantidad de veces que aparecio.
+            print(char, '->', c)
+            
+    print(f"Hubieron {contadorLetras} letras en total en su archivo.")
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerror(e.errno))
