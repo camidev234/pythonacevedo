4a490d65988f245ebe099008dc4c7e092c64bb71
juand <juandiegoimbachihoyos@gmail.com>
2023-07-31 16:13:18 -0500

    manejo archivos ejercicio

--- archivos/ejercicioUno.py
+++ archivos/ejercicioUno.py
@@ -17,23 +17,20 @@ try:
                 def crearArchivo(filename):
                     if filename not in listaArchivos:
                         listaArchivos.append(filename)
-                        file = open(filename, 'wt')
-                        file.close()
-                        stream = open(filename, 'rt')
-                        lineas = []
-                        l = stream.readlines()
-                        for i in l:
-                             lineas.append(i)
-                        stream.close()
-                        print("Su archivo tiene:", len(l), "lineas")
+                        stream = open(filename, 'wt')
+                        with open(filename, 'rt')as stream:
+                            lineas = []
+                            l = stream.readlines()
+                            for i in l:
+                                lineas.append(i)
+                            print("Su archivo tiene:", len(l), "lineas")
                     else:
-                        stream = open(filename, 'rt')
-                        lineas = []
-                        l = stream.readlines()
-                        for i in l:
-                             lineas.append(i)
-                        stream.close()
-                        print("Su archivo tiene:", len(l), "lineas")
+                        with open(filename, 'rt') as file:
+                            lineas = []
+                            l = file.readlines()
+                            for i in l:
+                                lineas.append(i)
+                            print("Su archivo tiene:", len(l), "lineas")
                     
                     
 
@@ -48,13 +45,13 @@ try:
                         documento = input("Digite su numero de documento: \n")
                         residencia = input("Escriba su residencia: ")
 
-                        file = open(archivo, 'a')
+                        with open(archivo, 'a') as file:
 
-                        cadena = nombre + "\n" + documento + "\n" + residencia + "\n"
-                        file.write(cadena)
-                        file.close()
+                            cadena = nombre + "\n" + documento + "\n" + residencia + "\n"
+                            file.write(cadena)
+                            file.close()
 
-                        print("Contenido insertado con exito.")
+                            print("Contenido insertado con exito.")
                         
                     insertarContenido(archivo)
 
@@ -63,13 +60,13 @@ try:
                     print("No hay archivos creados.")
                 else:
                     contChar = 0
-                    file = open(archivo, 'r')
-                    for char in file.read():
-                        if char == "\n":
-                            continue
-                        else:
-                            contChar +=1
+                    with open(archivo, 'r')as file:
+                        for char in file.read():
+                            if char == "\n":
+                                continue
+                            else:
+                                contChar +=1
 
-                    print(f"Su archivo tiene {contChar} caracteres")
+                        print(f"Su archivo tiene {contChar} caracteres")
 except IOError as e:
 	print("Se produjo un error de E/S:", strerror(e.errno))
